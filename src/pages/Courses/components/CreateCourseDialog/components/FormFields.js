a1c1ab0cb040fd608d03acb8e62adb20ff56af61
Peter Arnold <peter@cagefreeclimbing.com>
2019-04-26 16:00:02 -0400

    good

--- src/pages/Courses/components/CreateCourseDialog/components/FormFields.js
+++ src/pages/Courses/components/CreateCourseDialog/components/FormFields.js
@@ -5,6 +5,7 @@ import { Field, Form } from "formik";
 import { TextField, Select } from "formik-material-ui";
 
 //material-ui
+import { makeStyles } from "@material-ui/styles";
 import { Button } from "@material-ui/core";
 import MenuItem from "@material-ui/core/MenuItem";
 import InputLabel from "@material-ui/core/InputLabel";
@@ -13,15 +14,32 @@ import FormControl from "@material-ui/core/FormControl";
 import DialogContent from "@material-ui/core/DialogContent";
 import DialogActions from "@material-ui/core/DialogActions";
 
+//material-ui styles - custom hook
+const useStyles = makeStyles(
+  theme => ({
+    formControl: {
+      minWidth: 120,
+      marginTop: theme.spacing(1)
+    }
+  }),
+  { withTheme: true }
+);
+
 //main function
 const FormFields = props => {
   //destructure props
   const { isValid, handleDialogClose, subjects } = props;
 
+  //set course select label width
   const [labelWidth, setLabelWidth] = useState(0);
 
+  //set course select ref
   const inputLabelRef = useRef();
 
+  //use material-ui styles - custom hook
+  const classes = useStyles();
+
+  //effect hook
   React.useEffect(() => {
     setLabelWidth(ReactDOM.findDOMNode(inputLabelRef.current).offsetWidth);
     console.log("set label width");
@@ -40,18 +58,26 @@ const FormFields = props => {
           autoFocus
           fullWidth
         />
-        <br />
-        <br />
-        <br />
-        <br />
-        <FormControl variant="outlined">
-          <InputLabel ref={inputLabelRef} htmlFor="subject-select" />
+        <FormControl variant="outlined" className={classes.formControl}>
+          <InputLabel ref={inputLabelRef} htmlFor="subject-select">
+            Courses
+          </InputLabel>
           <Field
             type="text"
             name="subject"
             component={Select}
+            input={
+              <OutlinedInput
+                labelWidth={labelWidth}
+                name="subject"
+                id="subject-select"
+              />
+            }
             inputProps={{ name: "subject", id: "subject" }}
           >
+            <MenuItem key="0" value="">
+              Select None
+            </MenuItem>
             {subjects.map(subject => (
               <MenuItem key={subject.id} value={subject.id}>
                 {subject.name}

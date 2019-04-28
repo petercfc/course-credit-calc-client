3953b12bc19593dd62c17319e9c491067b387fb9
Peter Arnold <peter@cagefreeclimbing.com>
2019-04-28 13:18:39 -0400

    font?>

--- src/configs/withTheme.js
+++ src/configs/withTheme.js
@@ -13,7 +13,7 @@ const theme = createMuiTheme({
     fontFamily: [
       "-apple-system",
       "BlinkMacSystemFont",
-      "IBM Plex Sans",
+      // "IBM Plex Sans",
       '"Segoe UI"',
       "Roboto",
       '"Helvetica Neue"',
